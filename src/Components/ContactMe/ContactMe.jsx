70d5cdfc4af77d18c79b381d11f8db3f98dccbf0
Brandon <crandon1327@gmail.com>
2024-08-19 13:32:35 -0500

    form fix?

--- src/Components/ContactMe/ContactMe.jsx
+++ src/Components/ContactMe/ContactMe.jsx
@@ -1,8 +1,8 @@
 export const ContactMe = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    event.target.submit();
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   event.target.submit();
+  // };
 
   return (
     <div>
@@ -16,40 +16,62 @@ export const ContactMe = () => {
         </h2>
 
         <form
-          name="contact"
+          name="contact form"
           method="POST"
           data-netlify="true"
           netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
+          onSubmit="submit"
           netlify
           className="text-black flex flex-col justify-center gap-4 p-4 mt-5 xl:w-[50%] w-[100%] sm:w-[90%] md:w-[90%]"
         >
-          <input type="hidden" name="form-name" value="contact"></input>
+          <input type="hidden" name="form-name" value="contact form"></input>
+
           <p className="hidden">
             <label>
               Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
           </p>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            className="p-4 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
-            required={true}
-          />
-          <input
-            className="p-4 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            required={true}
-          />
-          <textarea
-            name="message"
-            className="h-40 p-4 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
-            placeholder="Write your message here..."
-            required={true}
-          ></textarea>
+
+          <div className="flex flex-col">
+            <label className="font-bold text-white" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter your name"
+              className="p-4 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+              required={true}
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="font-bold text-white" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="p-4 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              required={true}
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="font-bold text-white" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="h-40 p-4 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+              placeholder="Write your message here..."
+              required={true}
+            ></textarea>
+          </div>
           <button
             type="submit"
             className="p-3 font-bold text-black bg-accent rounded-xl"
